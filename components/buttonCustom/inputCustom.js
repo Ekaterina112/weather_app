f7c9362e4c9deaf265b4ef4b2ed81a0e4ca684bb
Ekaterina <ekaterinan112@gmail.com>
2021-04-14 12:16:13 +0300

    page weather ready

--- components/buttonCustom/inputCustom.js
+++ components/buttonCustom/inputCustom.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./inputCustom.module.css"
 
-export function InputCustom ({type,placeholder}) {
-    return <input type={type} placeholder={placeholder} className={styles.customInput}/>
+export function InputCustom ({type,placeholder,name,value,onChange}) {
+
+    return <div>
+        <input type={type} placeholder={placeholder} className={styles.customInput} name={name} autoComplete={'off'} value={value} onChange={onChange}/>
+    </div>
+
 }
--- pages/weather.js
+++ pages/weather.js
@@ -1,50 +1,115 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import styles from './../styles/weather.module.css'
 import {MainLayout} from "../components/MainLayout";
 import {InputCustom} from "../components/buttonCustom/inputCustom";
 import ButtonCustom from "../components/buttonCustom/buttonCustom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import {faSun} from "@fortawesome/free-solid-svg-icons"; // import the icons you need
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {fas} from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
+
 library.add(fas);
 
 
-//delete after using
 const API_key = "cec9a26ad731dd80c87fe414f1dc7256"
 
+export default function Weather({initialWeather}) {
 
-export default function Weather({startWeather}) {
+    let [startWeather, startWeatherSet] = useState(initialWeather)
+    let [weatherIcon, weatherIconSet] = useState("sun")
+    let [error, setError] = useState('')
 
-    /*
-        useEffect(
-            async function getstartWeather() {
-                const response = await fetch('')
-                const startWeather = await response.json()
-            }, []
-        )
-    */
+
+    useEffect(() => {
+        getWeatherIcon(startWeather.weather[0].id)
+    }, [startWeather.weather[0].id])
+
+    function getWeatherIcon(id) {
+        switch (true) {
+            case id >= 200 && id <= 232:
+                weatherIconSet("cloud-rain")
+                break
+            case id >= 300 && id <= 321:
+                weatherIconSet("cloud-rain")
+                break
+            case id >= 500 && id <= 531:
+                weatherIconSet("cloud-rain")
+                break
+            case id >= 600 && id <= 622:
+                weatherIconSet("snowflake")
+                break
+            case id >= 700 && id <= 781:
+                weatherIconSet("smog")
+                break
+            case id >= 800 && id <= 804:
+                weatherIconSet("cloud")
+                break
+            default:
+                weatherIconSet("rainbow")
+
+        }
+    }
+
+    //control input
+    const [inputState, setInputState] = useState({})
+    const inputHandler = (e) => {
+        setInputState(prev => {
+            return (
+                {
+                    ...prev,
+                    [e.target.name]: e.target.value
+                }
+            )
+        })
+    }
+
+    const getWeatherHandler = async (e) => {
+        e.preventDefault();
+        //const city = e.target.elements.city.value
+        //const country = e.target.elements.country.value
+        //const city = inputState.city
+        //const country = inputState.country
+
+        if (inputState.city && inputState.country) {
+            const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputState.city},${inputState.country}&units=metric&appid=${API_key}`)
+            if (response.status === 200) {
+                const start = await response.json();
+                startWeatherSet(start)
+                setError('')
+            } else {
+                //e.target.elements.city.value = ""
+                //e.target.elements.country.value = ""
+                setInputState({country: '', city: ''})
+                setError('Your response is not valid. \n Please, try again')
+            }
+        } else {
+            setError('Please, fill all fields')
+        }
+    };
+
 
     return <MainLayout>
         <div className={styles.weatherContainer}>
-            <form className={styles.formStyle}>
-                <InputCustom type={'text'} placeholder={'country'}/>
-                <InputCustom type={'text'} placeholder={'city'}/>
+            <form className={styles.formStyle} onSubmit={getWeatherHandler}>
+                <InputCustom value={inputState.country} onChange={inputHandler} type={'text'} placeholder={'country'}
+                             name={'country'}/>
+                <InputCustom value={inputState.city} onChange={inputHandler} type={'text'} placeholder={'city'}
+                             name={'city'}/>
                 <ButtonCustom title={'find'}/>
             </form>
-            <div className={styles.weatherContainer}>
-                <h1>London</h1>
+            <div className={styles.blockError}>{error && error}</div>
+            <div className={styles.weatherBlock}>
+                <h1>{startWeather.sys.country}, {startWeather.name}</h1>
                 <div className={styles.icon}>
-                    <FontAwesomeIcon icon={["fas", "sun"]} className={styles.icon}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={["fas", `${weatherIcon}`]}
+                                     className={styles.icon}/>
                 </div>
-                <p>{startWeather.sys.country}, {startWeather.name}</p>
                 <h2>{startWeather.main.temp}&deg;</h2>
                 <div>
                     <span>{startWeather.main.temp_min}&deg;-{startWeather.main.temp_max}&deg;</span>
                 </div>
-                <h3>Slow Rain</h3>
+                <h3>{startWeather.weather[0].description}</h3>
             </div>
         </div>
     </MainLayout>
@@ -53,10 +118,11 @@ export default function Weather({startWeather}) {
 //initial state in next.js
 //Static method, doing on a server
 Weather.getInitialProps = async () => {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${API_key}`)
-    const startWeather = await response.json()
-    console.log(startWeather)
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Gomel&units=metric&appid=${API_key}`)
+    const initialWeather = await response.json()
     return {
-        startWeather
+        initialWeather
     }
 }
+
+
